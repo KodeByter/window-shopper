3f84617f6e12226eecc13f4b04da71f542dc58a7
Jared Schneedle <jaredschneedle@jareds-imac.local>
2018-12-31 23:39:45 -0500

    Calc button and custom text fields added.

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcButton.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcButton
+        priceTxt.inputAccessoryView = calcButton
     }
-
-
+    
+    @objc func calculate() {
+        
+    }
+    
 }
 
